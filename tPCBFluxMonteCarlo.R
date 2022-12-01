b5ae695aab9f2fd6ac849515af6e78f78962db97
martie <andres-martinez@uiowa.edu>
2022-12-01 10:43:24 -0600

    Include different concentrations scenarios, from average and gm of all samples, to average of individual locations

--- tPCBFluxMonteCarlo.R
+++ tPCBFluxMonteCarlo.R
@@ -89,17 +89,73 @@ wc.raw <- read.csv("WaterConcentration.csv")
 wc.1 <- subset(wc.raw, select = -c(SampleID:Units))
 wc.2 <- cbind(wc.raw$LocationID, wc.1)
 colnames(wc.2)[1] <- "LocationID"
+# (1) All samples
+# (1) Mean and standard deviation
 wc.ave <- sapply(wc.1, mean, na.rm = TRUE)
 wc.sd <- sapply(wc.1, sd, na.rm = TRUE)
-wd.3 <- data.frame(t(rbind(wc.ave, wc.sd)))
-C.PCB.water.mean <- wd.3$wc.ave
-C.PCB.water.error <- wd.3$wc.sd
-
-# (1) Mean and standard deviation
-
-# (2) Geometric mean and geometric standard deviation
-
-# (3) Selected samples
+wc.3 <- data.frame(t(rbind(wc.ave, wc.sd)))
+C.PCB.water.mean <- wc.3$wc.ave
+C.PCB.water.sd <- wc.3$wc.sd
+# (II) Geometric mean and geometric standard deviation
+# Log 10 individual PCBs
+wc.log <- log10(wc.1)
+wc.gm <- exp(sapply(wc.log, mean, na.rm = TRUE))
+wc.gsd <- exp(sapply(wc.log, sd, na.rm = TRUE))
+wc.4 <- data.frame(t(rbind(wc.gm, wc.gsd)))
+C.PCB.water.gm <- wc.4$wc.gm
+C.PCB.water.gsd <- wc.4$wc.gsd
+# (2) Especific site
+# Selected site
+wc.POH001 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH001', ]
+wc.POH002 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH002', ]
+wc.POH003 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH003', ]
+wc.POH004 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH004', ]
+wc.POH005 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH005', ]
+wc.POH006 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH006', ]
+wc.POH007 <- wc.2[wc.2$LocationID == 'WCPCB_OR-POH007', ]
+# Calculate mean and sd for each site
+# WCPCB_OR-POH001
+wc.POH001.ave <- sapply(wc.POH001[, 2:160], mean, na.rm = TRUE)
+wc.POH001.sd <- sapply(wc.POH001[, 2:160], sd, na.rm = TRUE)
+wc.POH001.2 <- data.frame(t(rbind(wc.POH001.ave, wc.POH001.sd)))
+C.PCB.water.POH001.mean <- wc.POH001.2$wc.POH001.ave
+C.PCB.water.POH001.sd <- wc.POH001.2$wc.POH001.sd
+# WCPCB_OR-POH002
+wc.POH002.ave <- sapply(wc.POH002[, 2:160], mean, na.rm = TRUE)
+wc.POH002.sd <- sapply(wc.POH002[, 2:160], sd, na.rm = TRUE)
+wc.POH002.2 <- data.frame(t(rbind(wc.POH002.ave, wc.POH002.sd)))
+C.PCB.water.POH002.mean <- wc.POH002.2$wc.POH002.ave
+C.PCB.water.POH002.sd <- wc.POH002.2$wc.POH002.sd
+# WCPCB_OR-POH003
+wc.POH003.ave <- sapply(wc.POH003[, 2:160], mean, na.rm = TRUE)
+wc.POH003.sd <- sapply(wc.POH003[, 2:160], sd, na.rm = TRUE)
+wc.POH003.2 <- data.frame(t(rbind(wc.POH003.ave, wc.POH003.sd)))
+C.PCB.water.POH003.mean <- wc.POH003.2$wc.POH003.ave
+C.PCB.water.POH003.sd <- wc.POH003.2$wc.POH003.sd
+# WCPCB_OR-POH004
+wc.POH004.ave <- sapply(wc.POH004[, 2:160], mean, na.rm = TRUE)
+wc.POH004.sd <- sapply(wc.POH004[, 2:160], sd, na.rm = TRUE)
+wc.POH004.2 <- data.frame(t(rbind(wc.POH004.ave, wc.POH004.sd)))
+C.PCB.water.POH004.mean <- wc.POH004.2$wc.POH004.ave
+C.PCB.water.POH004.sd <- wc.POH004.2$wc.POH004.sd
+# WCPCB_OR-POH005
+wc.POH005.ave <- sapply(wc.POH005[, 2:160], mean, na.rm = TRUE)
+wc.POH005.sd <- sapply(wc.POH005[, 2:160], sd, na.rm = TRUE)
+wc.POH005.2 <- data.frame(t(rbind(wc.POH005.ave, wc.POH005.sd)))
+C.PCB.water.POH005.mean <- wc.POH005.2$wc.POH005.ave
+C.PCB.water.POH005.sd <- wc.POH005.2$wc.POH005.sd
+# WCPCB_OR-POH006
+wc.POH006.ave <- sapply(wc.POH006[, 2:160], mean, na.rm = TRUE)
+wc.POH006.sd <- sapply(wc.POH006[, 2:160], sd, na.rm = TRUE)
+wc.POH006.2 <- data.frame(t(rbind(wc.POH006.ave, wc.POH006.sd)))
+C.PCB.water.POH006.mean <- wc.POH006.2$wc.POH006.ave
+C.PCB.water.POH006.sd <- wc.POH006.2$wc.POH006.sd
+# WCPCB_OR-POH007
+wc.POH007.ave <- sapply(wc.POH007[, 2:160], mean, na.rm = TRUE)
+wc.POH007.sd <- sapply(wc.POH007[, 2:160], sd, na.rm = TRUE)
+wc.POH007.2 <- data.frame(t(rbind(wc.POH007.ave, wc.POH007.sd)))
+C.PCB.water.POH007.mean <- wc.POH007.2$wc.POH007.ave
+C.PCB.water.POH007.sd <- wc.POH007.2$wc.POH007.sd
 
 # Meteorological data -----------------------------------------------------
 
@@ -135,7 +191,7 @@ T <- 298.15 # [K]
 
 F.PCB.aw <- NULL
 # number of replicates for Monte Carlo simulation
-for (replication in 1:1000) {
+for (replication in 1:100) {
 
 # Random parameters
 # Parameters for calculating Delta Uaw
@@ -151,7 +207,8 @@ H0 <- rnorm(1, H0.mean, H0.error) # [Pa m3/mol]
 # Octanol-water partition coefficient
 Kow <- rnorm(1, Kow.mean, Kow.error) # [Lwater/Loctanol] 
 # PCB water concentration
-C.PCB.water <- abs(rnorm(1, C.PCB.water.mean, C.PCB.water.error)) # [pg/L]
+# Concentrations can be changed
+C.PCB.water <- abs(rnorm(1, C.PCB.water.gm, C.PCB.water.gsd)) # [pg/L]
 # DOC (Spencer et al 2012)
 DOC <- abs(rnorm(1, 2, 0.3)) # [mg/L]
 # Water temperature
