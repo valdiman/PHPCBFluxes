b63045785da14fd10141989aebe70636ea69a86c
Martinez Araneda <martie@uiowa.edu>
2025-05-08 12:52:35 -0500

    updated version

--- R/CorrelationDistPCB.R
+++ R/CorrelationDistPCB.R
@@ -21,24 +21,33 @@ p_label <- paste0("p-value = ", signif(p_value_a, 2))
 
 # New distance values to make the prediction looks smoother in plot
 dis_smooth <- seq(min(ph$dis), max(ph$dis), length.out = 500)
+
 # Prediction
 tpcb_fit_smooth <- predict(fit.tpcb, newdata = data.frame(dis = dis_smooth))
+
 # Create a new data for smoothed predictions
 smoothed_data <- data.frame(dis = dis_smooth, tpcb_fit = tpcb_fit_smooth)
 
 # Plot
-ggplot(ph, aes(x = dis, y = tpcb)) + # need to change tpcb
-  geom_point(size = 3) +  # Plot the actual data points
-  geom_line(data = smoothed_data, aes(x = dis, y = tpcb_fit), color = "blue", size = 1) +  # Smoother predicted line
+plotTpcb <- ggplot(ph, aes(x = dis, y = tpcb)) + # need to change tpcb
+  geom_point(size = 3, shape=21) + # Plot the actual data points
+  geom_line(data = smoothed_data,
+            aes(x = dis, y = tpcb_fit), color = "blue", size = 1) + # Smoother predicted line
+  geom_text(aes(label = id), vjust = 2, size = 3) +
   theme_bw() +
-  annotate("text", x = Inf, y = 950,
+  xlim(0,2501)+
+  annotate("text", x = Inf, y = Inf,
            label = paste0("R² = ", round(r_squared, 2)),
-           hjust = 1.1, vjust = 2.5, size = 5) +
-  annotate("text", x = Inf, y = 900, label = p_label,
-           hjust = 1.1, vjust = 1.5, size = 5) +
+           hjust = 1.1, vjust = 3, size = 5) +
   xlab(expression(bold("Distance to water (m)"))) +
-  ylab(expression(bold(Sigma~PCB~(pg/m^3)))) + # need to change the name
+  ylab(expression(bold(tpcb~(pg/m^3)))) + # need to change the name
+  theme(aspect.ratio = 1) +
   theme(axis.text.y = element_text(face = "bold", size = 14),
         axis.title.y = element_text(face = "bold", size = 14)) +
   theme(axis.text.x = element_text(face = "bold", size = 14),
         axis.title.x = element_text(face = "bold", size = 14))
+
+plotTpcb
+
+ggsave("Rcode/plots/plotTPCB.png", plot = plotTpcb, width = 5,
+       height = 5, dpi = 500)
